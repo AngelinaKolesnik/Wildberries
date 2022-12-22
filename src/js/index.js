5253286673394d90185af6ff7b0c03d5afe2e5bf
SakovichElena <lenkasakovich.le@gmail.com>
2022-12-22 20:29:02 +0300

    add adaptive

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,19 @@
-console.log("S.E")
+// console.log("S.E")
+const inputEnter = document.getElementById("input-enter");
+inputEnter.value = ""; 
+inputEnter.focus();
+
+// inputEnter.addEventListener("keydown", function(e) {
+//     if(e.code === "Enter") {
+//         addTask(e);
+//       }
+// });
+
+// function addTask(e) {
+//     e.preventDefault();
+//     if (inputEnter.value.trim() === "") {
+//       alert("Поле ввода не должно быть пустым!");
+//       inputEnter.value = "";
+//       return;
+//     } 
+// }
