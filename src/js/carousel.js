acb2bad8a668adaf2814c08cf54ddf830d90abbb
DaryaSolomennikova <solomka306@gmail.com>
2023-01-16 17:29:14 +0300

    refactor html/css and add js (indicator active)

--- src/js/carousel.js
+++ src/js/carousel.js
@@ -3,7 +3,7 @@ const slides = Array.from(track.children);
 const nextBtn = document.querySelector('.carousel__button--right');
 const prevBtn = document.querySelector('.carousel__button--left');
 const navIndicate = document.querySelector('.carousel__nav');
-const dots = Array(navIndicate.children);
+const dots = Array.from(navIndicate.children);
 
 const slideWidth = slides[0].getBoundingClientRect().width;
 
@@ -16,14 +16,14 @@ slides.forEach(setSlidePosition);
 
 const moveToSlide = (track,currentSlide,targetSlide) => {
     track.style.transform = 'translateX(-' + targetSlide.style.left + ')'; 
-    currentSlide.classList.remove('current-slide');
-    targetSlide.classList.add('current-slide');
+    currentSlide.classList.remove('active');
+    targetSlide.classList.add('active');
 }
 
 //клик влево - переход слайда влево
 
 prevBtn.addEventListener('click', e => {
-    const currentSlide = track.querySelector('.current-slide');
+    const currentSlide = track.querySelector('.active');
     const prevSlide = currentSlide.previousElementSibling;
     
     moveToSlide(track,currentSlide,prevSlide)
@@ -32,10 +32,24 @@ prevBtn.addEventListener('click', e => {
 //клик вправо - переход слайда вправо
 
 nextBtn.addEventListener('click', e => {
-    const currentSlide = track.querySelector('.current-slide');
+    const currentSlide = track.querySelector('.active');
     const nextSlide = currentSlide.nextElementSibling;
     
     moveToSlide(track,currentSlide,nextSlide)
 })
 
 //клик на индикатор, переход к слайду
+
+navIndicate.addEventListener('click', e =>{
+    targetDot = e.target.closest('button');
+
+    if(!targetDot) return;
+
+    const currentSlide = track.querySelector('.active');
+    const currentDot = navIndicate.querySelector('.active');
+    const targetIndex = dots.findIndex(dot => dot === targetDot);
+    const targetSlide = slides[targetIndex];
+
+    moveToSlide(track,currentSlide,targetSlide);
+
+})
